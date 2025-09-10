6c05bbbf86724b9b8550c311b8e39e21f90df77a
Bre01cc <boliveiraassisreis@gmail.com>
2025-09-10 18:53:31 -0300

    lógica do butão funcionando

--- app.js
+++ app.js
@@ -9,29 +9,36 @@ async function buscarImagens(texto) {
     const imagem = await response.json()
     return imagem.message
 }
-    function criarImg(imagem) {
+function criarImg(imagem) {
     const container = document.getElementById('container')
     const foto = document.createElement('div')
     foto.classList.add('foto')
     const img = document.createElement('img')
     // let texto = document.getElementById('caixa-texto').value
     // const imagens = await buscarImagens()
-    img.src= imagem
+    img.src = imagem
     foto.appendChild(img)
     container.appendChild(foto)
 }
 
 async function carregar(texto) {
+    const container = document.getElementById('container');
+    container.innerHTML = ''; // limpa imagens anteriores
     const imagens = await buscarImagens(texto)
-    imagens.forEach(criarImg)
+    imagens.forEach(criarImg);
 }
-carregar()
+
 
 const botaoBuscar = document.getElementById('buscar')
 
-botaoBuscar.addEventListener('onclick',()=>{
+botaoBuscar.addEventListener('click', () => {
     const input = document.getElementById('caixa-texto').value
-    carregar(input)
+    if (!input) {
+
+    } else {
+        carregar(input)
+    }
+
 })
 // buscarImagens('')
 //    const arrayImagens = await buscarImagens()
